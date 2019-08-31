4a4cc227cc5829aa56d984a1ed98b57b5d05b636
pkvjcet <princekurian2000@gmail.com>
2019-08-31 15:08:01 +0530

    event handling

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+function readValues(){
+    var username=document.getElementById('username').value;
+    var pwd=document.getElementById('pwd').value;
+    console.log(username);
+    console.log(pwd);
+}
