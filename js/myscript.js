20ace814dfd635e69d274838b51294ce84981134
pkvjcet <princekurian2000@gmail.com>
2019-08-31 15:36:22 +0530

    event handled add two numbers

--- js/myscript.js
+++ js/myscript.js
@@ -4,3 +4,12 @@ function readValues(){
     console.log(username);
     console.log(pwd);
 }
+function add(){
+    var num1=parseInt(document.getElementById('num1').value);
+    var num2=parseInt(document.getElementById('num2').value);
+    var sum=num1+num2;
+    console.log("sum is");
+    console.log(sum);
+    document.getElementById("result").innerHTML=sum;
+    
+}
