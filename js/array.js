170ca6403af8b5b95be32acb064f34b876d30126
pkvjcet <princekurian2000@gmail.com>
2019-08-31 14:27:14 +0530

    upto array

---
+++ js/array.js
@@ -0,0 +1,4 @@
+let students=["Tom","Jerry","rahul","anoop"];
+console.log(students);
+console.log(students[2]);
+console.log(students.length);
---
+++ js/arrayobject.js
@@ -0,0 +1,11 @@
+let students=[
+    {
+        "name":"manu",
+        "age":23
+    },
+    {
+        "name":"pk",
+        "age":35
+    }
+];
+console.log(students[0].name);
